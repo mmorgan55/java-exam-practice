44ec1a7a537d802d9d65682b2cda56ddf47f3bf7
Matthew Morgan <mmorgan55@cnm.edu>
2019-01-08 13:42:42 -0700

    Method reference implementation for consumer

--- src/edu/cnm/deepdive/WeirdStream.java
+++ src/edu/cnm/deepdive/WeirdStream.java
@@ -11,7 +11,7 @@ public class WeirdStream {
   public static void main(String[] args) {
     IntStream.generate(new RandomSupplier())
         .limit(1000)
-        .forEach(value -> System.out.println(value));
+        .forEach(System.out::println);
   }
 
   private static class RandomSupplier implements IntSupplier {
@@ -19,7 +19,7 @@ public class WeirdStream {
     private Random rng;
 
     {
-      rng = new SecureRandom();
+      rng = new Random(-1); // Reproducible sequence
     }
 
     @Override
