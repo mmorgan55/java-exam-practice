aadf3c6fd459a80144f8ff2af35bb2aaf36c89ea
Matthew Morgan <mmorgan55@cnm.edu>
2019-01-08 13:38:52 -0700

    First lambda implementation

--- src/edu/cnm/deepdive/WeirdStream.java
+++ src/edu/cnm/deepdive/WeirdStream.java
@@ -11,12 +11,7 @@ public class WeirdStream {
   public static void main(String[] args) {
     IntStream.generate(new RandomSupplier())
         .limit(1000)
-        .forEach(new IntConsumer() {
-          @Override
-          public void accept(int value) {
-            System.out.println(value);
-          }
-        });
+        .forEach(value -> System.out.println(value));
   }
 
   private static class RandomSupplier implements IntSupplier {
